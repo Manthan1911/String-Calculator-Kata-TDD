5a7e1ac902910f9eeee52b413d7604834a8d124b
Manthan1911 <patelmanthan2000@gmail.com>
2022-08-20 23:21:23 +0530

    refactor: add instiantiateClassStringCalculator() method with @Before annotation in StringCalculatorTest class
    this method will instantiate StringCalculator class' object everytime before running each test

--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -1,17 +1,23 @@
 import org.junit.Test;
+import org.junit.Before;
 import static org.junit.Assert.assertEquals;
 
 public class StringCalculatorTest {
 
+    private StringCalculator strCalc;
+
+    @Before
+    public void instantiateClassStringCalculator() {
+        strCalc = new StringCalculator();
+    }
+
     @Test
     public void shouldReturnZeroIfEmptyStringIsProvided() {
-        StringCalculator strCalc = new StringCalculator();
         assertEquals("if empty string is provided than add() method should return 0", 0, strCalc.add(""));
     }
 
     @Test
     public void shouldReturnNumberItselfIfOneNumberIsProvided() {
-        StringCalculator strCalc = new StringCalculator();
         assertEquals("if only one number is provided then add() method should return that number itself", 100,
                 strCalc.add("100"));
     }
