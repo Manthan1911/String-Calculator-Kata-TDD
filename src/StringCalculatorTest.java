96b6c1514861b0f85afa866c84d380e92cfbade8
Manthan1911 <patelmanthan2000@gmail.com>
2022-08-20 22:39:18 +0530

    test: method add() returns number itself
    if only one number is provided as argument then add() method will return that number itself

--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -9,4 +9,10 @@ public class StringCalculatorTest {
         assertEquals("if empty string is provided than add() method should return 0", 0, strCalc.add(""));
     }
 
+    @Test
+    public void shouldReturnNumberItselfIfOneNumberIsProvided() {
+        StringCalculator strCalc = new StringCalculator();
+        assertEquals("if only one number is provided then add() method should return that number itself", 100,
+                strCalc.add("100"));
+    }
 }
