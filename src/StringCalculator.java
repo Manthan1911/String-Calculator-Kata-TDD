c7874fc92a006ca662801d476bd8bff28ef96e9c
Manthan1911 <patelmanthan2000@gmail.com>
2022-08-21 00:04:58 +0530

    refactor: add a condition for comma seperated values in add() of StringCalculator

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,13 +1,19 @@
+import javax.lang.model.element.Element;
+
 class StringCalculator {
 
     public int add(String str) {
         if (str.equals("")) {
             return 0;
         }
-        if (str.length() > 0) {
-            return Integer.parseInt(str);
+        if (str.contains(",")) {
+            String[] splittedStr = str.split(",");
+            int sum = 0;
+            for (String currEl : splittedStr) {
+                sum += Integer.parseInt(currEl);
+            }
+            return sum;
         }
-        return 1;
+        return Integer.parseInt(str);
     }
-
 }
